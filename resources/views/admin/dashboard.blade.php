2c52d39ecab850077b6c6eec577bacabf4a8bfc2
nassa-fadilla <nssfadilla.26@gmail.com>
2026-05-07 16:47:51 +0700

    admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -51,7 +51,7 @@
                     d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0h6" />
                 </svg>
               </span>
-              <span>Dashboard Admin</span>
+              <span>Dashboard Guru</span>
             </h1>
 
             <p class="mt-2 text-sm text-slate-500">
