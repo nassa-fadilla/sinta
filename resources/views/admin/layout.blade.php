437d20e96f37aac82b333ddd71945ec51c8efc84
nassa-fadilla <nssfadilla.26@gmail.com>
2026-05-07 17:34:33 +0700

    favikon

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,6 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title', 'Admin • SINTA')</title>
+  <link rel="icon" href="{{ asset('images/logo-sma2.png') }}">
+  <script src="https://cdn.tailwindcss.com"></script>
 
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,12 +1,17 @@
- <!doctype html>
+<!doctype html>
 <html lang="id">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>@yield('title','SINTA')</title>
-  @vite(['resources/css/app.css','resources/js/app.js'])
+  <title>@yield('title', 'SINTA')</title>
+  <link rel="icon" href="{{ asset('images/logo-sma2.png') }}">
+  <script src="https://cdn.tailwindcss.com"></script>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="antialiased text-gray-900">
   @yield('content')
 </body>
+
 </html>
